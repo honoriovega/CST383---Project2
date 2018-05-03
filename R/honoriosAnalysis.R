f01182366eb8052ba338f923219cfd5dc9c3b7eb
Honorio Vega <hvega@csumb.edu>
2018-05-03 15:59:35 -0700

    yeet

--- R/honoriosAnalysis.R
+++ R/honoriosAnalysis.R
@@ -1,3 +1,4 @@
 dat = read.csv( paste0(getwd(), "/data/Video_Game_Sales_as_of_Jan_2017.csv"))
-par(mar=c(1,2,2,1))
+par(mar=c(3,5,1,1))
+# botto, left, top ,right
 barplot(table(dat$Year_of_Release),horiz=TRUE,las=1)
